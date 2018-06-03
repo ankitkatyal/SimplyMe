971724145bffc440512cdef2f0060274d42a262b
katyal <katyal@ankits-air>
2018-06-03 14:22:25 +0530

    component scan and deleed old main class

--- SpringBootTwitterBootstrapWebjar/src/main/java/com/society/management/bootstrap/SpringBootTwitterBootstrapWebjarApplication.java
+++ SpringBootTwitterBootstrapWebjar/src/main/java/com/society/management/bootstrap/SpringBootTwitterBootstrapWebjarApplication.java
@@ -2,8 +2,10 @@ package com.society.management.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.society.management") 
 public class SpringBootTwitterBootstrapWebjarApplication {
 
 	public static void main(String[] args) {
