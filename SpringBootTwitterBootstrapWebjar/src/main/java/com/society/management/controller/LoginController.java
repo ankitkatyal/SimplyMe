66dec88ddcb07672b93f817e6edabcdc603e58f1
katyal <katyal@192.168.1.4>
2018-08-11 22:45:42 +0530

    Name change

--- SpringBootTwitterBootstrapWebjar/src/main/java/com/society/management/controller/LoginController.java
+++ SpringBootTwitterBootstrapWebjar/src/main/java/com/society/management/controller/LoginController.java
@@ -1,21 +1,13 @@
 package com.society.management.controller;
 
-import java.lang.reflect.InvocationTargetException;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -25,59 +17,30 @@ import com.society.management.entity.UserInfo;
 import com.society.management.service.UserServices;
 
 @Controller
-public class LoginController extends WebMvcConfigurerAdapter{
+public class LoginController1 extends WebMvcConfigurerAdapter {
+	
 	@Autowired
 	private UserServices userServices;
 	@Autowired
 	BCryptPasswordEncoder bCryptPasswordEncoder;
-	
 	@Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/HomeMenu").setViewName("HomeMenu");
-    }
-	@RequestMapping("/")
-	public String home(Model model) {
-		model.addAttribute("user", new User());
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/HomeMenu").setViewName("HomeMenu");
+	}
+
+	@GetMapping("/")
+	public String showForm(User user) {
 		return "videoLogin2";
 	}
-	
-	@PostMapping("/login")
-    public String validateLoginInfo(Model model, @Valid User loginForm, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "login";
-        }
-        model.addAttribute("user", loginForm.getEmail());
-        return "HomeMenu";
-    }
-	
-	@GetMapping("/login1")
-    public String showForm(User loginForm) {
-        return "videoLogin2";
-	}
 
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String Greeting(@ModelAttribute("user") User user, ModelMap userMap, BindingResult bindingResult, HttpServletRequest request) {
-		if(userServices.findByEmail(user.getEmail())!=null){
-			
-		}
-		if (bindingResult.hasErrors()) { 
-			System.out.println(bindingResult.getAllErrors());	
-			
-		}
-		userMap.addAttribute("username", user.getEmail());
-		userMap.addAttribute("password", user.getPassword());
-		return "HomeMenu";
-
-	}
-	
-	@RequestMapping(value = "/login1", method = RequestMethod.POST)
-	public ModelAndView Greeting( ModelAndView userMap,@ModelAttribute("user") @Valid User user, BindingResult bindingResult, HttpServletRequest request) throws IllegalAccessException, InvocationTargetException {
-		if(userServices.findByEmail(user.getEmail())!=null  ){
-			UserInfo existingUser = userServices.findByEmail(user.getEmail());
+	@PostMapping("/")
+	public ModelAndView validateLoginInfo(ModelAndView userMap, @Valid User user, BindingResult bindingResult) {
+		UserInfo existingUser = userServices.findByEmail(user.getEmail());
+		if(existingUser!=null  ){
 			if(existingUser.isEnabled()){
-				String encryptPassword = bCryptPasswordEncoder.encode(user.getPassword());
-				if(encryptPassword.equals(existingUser.getPassword())){
-					userMap.addObject("confirmationMessage", "Redirecting to homepage ");
+				boolean passwordMatch = bCryptPasswordEncoder.matches(user.getPassword(), existingUser.getPassword());
+				if(passwordMatch){
+					userMap.addObject("welcomeUser","Welcome " +existingUser.getFirstName()+ " "+existingUser.getLastName());
 					userMap.setViewName("HomeMenu");
 					return userMap;
 				}else{
@@ -92,6 +55,5 @@ public class LoginController extends WebMvcConfigurerAdapter{
 		}
 		userMap.setViewName("videoLogin2");
 		return userMap;
-
 	}
 }
